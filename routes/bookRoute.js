3362db90b384bacd2ef93c737441701c6384a108
Mo Ali <XCreativeMarketing@gmail.com>
2016-03-09 22:40:38 +0000

    Refactoring

--- routes/bookRoute.js
+++ routes/bookRoute.js
@@ -1,60 +1,63 @@
 var express = require('express');
 var bodyParser = require ('body-parser');
-var bookRouter = express.Router();
-var connection = require('../db/dbConnect');
-var Book = require('../models/bookModel');
 
 app = express();
 var jsonParser = bodyParser.json();
 
-bookRouter.route('/books')
-    .get(function(request, response){
-        var query = {};
-        switch(Object.keys(request.query)[0])
-        {
-            case 'genre': query.genre = request.query.genre;
-                break;
-            case 'author': query.author = request.query.author;
-                break;
-            case 'read': query.read = request.query.read;
-                break;
-            case 'title': query.title = request.query.title;
-                break;
-            default : // return all records
-                Book.find(function(err,books){
-                    if (err)
-                        response.send(err);
-                    else
-                        response.json(books);
-                });
-                return;
-        }
-        Book.find(query,function(err,books){
-            if (err)
-                response.send(err);
-            else
-                response.json(books);
-        });
-    })
-    .post(jsonParser,function(request, response){
-        var book = new Book(request.body);
-        if (!request.body)
-            return response.sendStatus(400);
-        book.save();
-        response.status(201).send(book);
-    });
-
-bookRouter.route('/books/:bookid')
-    .get(function(request, response){
-        var bookId = request.params.bookid;
-        if (bookId) {
-            Book.findById(bookId, function (err, book) {
+routes = function(Book) {
+    var bookRouter = express.Router();
+    bookRouter.route('/books')
+        .get(function (request, response) {
+            var query = {};
+            switch (Object.keys(request.query)[0]) {
+                case 'genre':
+                    query.genre = request.query.genre;
+                    break;
+                case 'author':
+                    query.author = request.query.author;
+                    break;
+                case 'read':
+                    query.read = request.query.read;
+                    break;
+                case 'title':
+                    query.title = request.query.title;
+                    break;
+                default : // return all records
+                    Book.find(function (err, books) {
+                        if (err)
+                            response.send(err);
+                        else
+                            response.json(books);
+                    });
+                    return;
+            }
+            Book.find(query, function (err, books) {
                 if (err)
-                    response.sendStatus(404);
+                    response.send(err);
                 else
-                    response.json(book);
+                    response.json(books);
             });
-        }
-    });
+        })
+        .post(jsonParser, function (request, response) {
+            var book = new Book(request.body);
+            if (!request.body)
+                return response.sendStatus(400);
+            book.save();
+            response.status(201).send(book);
+        });
 
-module.exports = bookRouter;
+    bookRouter.route('/books/:bookid')
+        .get(function (request, response) {
+            var bookId = request.params.bookid;
+            if (bookId) {
+                Book.findById(bookId, function (err, book) {
+                    if (err)
+                        response.sendStatus(404);
+                    else
+                        response.json(book);
+                });
+            }
+        });
+    return bookRouter;
+};
+module.exports = routes;
